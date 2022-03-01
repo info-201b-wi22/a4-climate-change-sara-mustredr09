e11169467267395b976f8b01807575b16b218198
Sára Mustre-del Río <saramr09@uw.edu>
2022-02-28 19:05:06 -0800

    add files

---
+++ app.R
@@ -0,0 +1,12 @@
+library(shiny)
+# install.packages(rsconnect)
+library(rsconnect)
+source("ui.R")
+source("server.R")
+
+
+# Add a dynamic title to the hot dog plot
+# Publish your Shiny App to the web
+
+
+shinyApp(ui = ui, server = server)
---
+++ server.R
@@ -0,0 +1,1 @@
+climate_change_data <- read.csv("https://raw.githubusercontent.com/owid/co2-data/master/owid-co2-data.csv")
